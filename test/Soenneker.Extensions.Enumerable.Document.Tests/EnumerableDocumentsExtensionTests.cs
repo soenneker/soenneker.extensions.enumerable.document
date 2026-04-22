dee1afb627377233294b634c52ac44f247a72f79
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:55 -0500

    TUnit conversion

--- test/Soenneker.Extensions.Enumerable.Document.Tests/EnumerableDocumentsExtensionTests.cs
+++ test/Soenneker.Extensions.Enumerable.Document.Tests/EnumerableDocumentsExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.Enumerable.Document.Tests;
 
 public sealed class EnumerableDocumentsExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
